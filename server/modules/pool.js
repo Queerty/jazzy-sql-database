a1a2366ef639a8ab0303e9c9742c393ab1789a07
Meghan Cleland <mplscleland@gmail.com>
2021-06-29 21:33:21 -0500

    set up pool js file and added db sql info

---
+++ server/modules/pool.js
@@ -0,0 +1,25 @@
+const pg= require('pg');
+const Pool = pg.Pool;
+const config = {
+    databas: 'jazzy-sql',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000,
+};
+//new pool instance to manage connections
+const pool = new Pool(config);
+
+//for debugging we check connections
+pool.on('connect', (client) => {
+    console.log('PostgeSQL connected');
+});
+
+//log errors
+pool.on('error', (err, client) => {
+    console.log('Unexpected error on idle client', err)
+});
+
+//access to pool in other code
+module.exports = pool;
+
